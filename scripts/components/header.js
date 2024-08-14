7234b185c38eaa4c59d181d084687708f662e1e2
caleb <calebsg2253284+git@gmail.com>
2024-08-13 22:34:09 -0400

    create basic navbar

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,5 +1,14 @@
+import helpers from "../helpers/helpers.js";
+import navbar from "./navbar.js";
+
 const header = document.createElement('header');
 header.id = 'header-component';
-header.innerText = "PLACEHOLDER_HEADER_COMPONENT";
+
+const logo = document.createElement('img');
+logo.id = 'generic-logo';
+logo.src = './assets/generic-logo-2.png';
+logo.alt = 'generic-logo';
+
+helpers.appendElements([logo, navbar], header);
 
 export default header;
---
+++ scripts/components/navbar.js
@@ -0,0 +1,18 @@
+import helpers from "../helpers/helpers.js";
+
+const navbar = document.createElement('nav');
+
+const mainTitles = ['Services', 'About', 'Contact', 'Join Today'];
+
+const elements = [];
+
+for (const title of mainTitles) {
+  const element = document.createElement('button');
+  element.id = `${title.toLowerCase().split(' ')[0]}-nav-button`;
+  element.innerText = title;
+  elements.push(element);
+}
+
+helpers.appendElements(elements, navbar);
+
+export default navbar;
