4498727b5381b08c3e9f0933c9913f44eec82e53
caleb <calebsg2253284+git@gmail.com>
2024-08-13 21:59:47 -0400

    add generic logo and favicon

--- scripts/helpers/helpers.js
+++ scripts/helpers/helpers.js
@@ -1,9 +1,12 @@
+// class of helper functions for direct DOM interaction
+
 class Helpers {
   appendElements = (elements, destination) => {
     for (const element of elements) {
       destination.appendChild(element);
     }
   }
+  
 }
 
 export default new Helpers;
